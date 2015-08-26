f906e9651a26bcf9fc1d68628d58cc5f90606eb0
Romain MOREAU <moreau.romain83@gmail.com>
2015-08-26 13:43:26 +0200

    Index template and Style ok

--- js/app.js
+++ js/app.js
@@ -4,14 +4,17 @@ angular.module("marvel_test", ["ui.router", "marvel_test.controllers", "marvel_t
         $stateProvider
             .state('home', {
                 url: "/home",
+                controller:"HomeCtrl",
                 templateUrl: "template/home.html"
             })
             .state('creator', {
                 url: "/creator",
+                controller:"CreatorCtrl",
                 templateUrl: "template/creator.html"
             })
             .state('comic', {
                 url: "/comic",
+                controller:"ComicCtrl",
                 templateUrl: "template/comic.html"
             })
         ;
--- js/controllers.js
+++ js/controllers.js
@@ -1,1 +1,11 @@
-angular.module("marvel_test.controllers", ["marvel_test.services"]);
+angular.module("marvel_test.controllers", ["marvel_test.services"])
+    .controller("HomeCtrl", function(){
+
+    })
+    .controller("CreatorCtrl", function(){
+
+    })
+    .controller("ComicCtrl", function(){
+
+    })
+;
