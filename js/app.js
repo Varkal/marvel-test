4b5563c14f893c342baff916db5875c5059bb63e
Romain MOREAU <moreau.romain83@gmail.com>
2015-08-26 12:22:35 +0200

    Router Configuration

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-angular.module("marvel_test", ["marvel_test.controllers", "marvel_test.services"]);
+angular.module("marvel_test", ["ui.router", "marvel_test.controllers", "marvel_test.services"])
+    .config(function($stateProvider, $urlRouterProvider){
+
+        $stateProvider
+            .state('home', {
+                url: "/home",
+                templateUrl: "template/home.html"
+            })
+            .state('creator', {
+                url: "/creator",
+                templateUrl: "template/creator.html"
+            })
+            .state('comic', {
+                url: "/comic",
+                templateUrl: "template/comic.html"
+            })
+        ;
+
+        $urlRouterProvider.otherwise("/home");
+    })
+;
