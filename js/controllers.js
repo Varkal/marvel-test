e18f30382ed598db0cf77ad77e6e675d288910a8
Romain MOREAU <moreau.romain83@gmail.com>
2015-08-27 00:34:28 +0200

    add spinner to wait during api loading

--- js/controllers.js
+++ js/controllers.js
@@ -2,22 +2,26 @@ angular.module("marvel_test.controllers", ["marvel_test.services"])
     .controller("HomeCtrl", function(){
 
     })
-    .controller("CreatorCtrl", function($scope, marvelApi){
+    .controller("CreatorCtrl", function($scope, marvelApi, $rootScope){
+
 
         $scope.currentPage = 1;
-        $scope.creatorsByPage = 50;
+        $scope.creatorsByPage = 20;
 
         $scope.getCreators = function(){
             console.log("Request");
+            $rootScope.loading=true;
             marvelApi.Request("creators").queryParam("orderBy", "firstName").limit($scope.creatorsByPage).offset(($scope.currentPage-1)*$scope.creatorsByPage).exec().then(
                 function onSuccess(result){
                     var datas = result.data;
                     console.log(datas);
                     $scope.creators = datas.data.results;
-                    $scope.total = datas.data.total
+                    $scope.total = datas.data.total;
+                    $rootScope.loading=false;
                 },
                 function onError(error){
                     alert(JSON.stringify(error));
+                    $rootScope.loading=false;
                 }
             )
         };
